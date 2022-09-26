9c10647a7fcd08a3dc29558da7d75f43e8cae451
Yasir-Khan22 <yasirkhanone22@gmail.com>
2022-09-27 00:46:01 +0500

    js added

--- js/main.js
+++ js/main.js
@@ -5,3 +5,9 @@ burger.addEventListener("click", () => {
     burger.classList.toggle("active")
     navMenu.classList.toggle("active")
 })
+
+
+document.querySelectorAll(".items").forEach(n => n.addEventListener("click", () => {
+    burger.classList.remove("active")
+    navMenu.classList.remove("active")
+}))
